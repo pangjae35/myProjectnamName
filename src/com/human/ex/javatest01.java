990d8eb60908d68693f8bc7f6c08a7a98987b8cf
주민재 <@121.166.32.84>
2022-03-03 14:26:24 +0900

    first commit

---
+++ src/com/human/ex/javatest01.java
@@ -0,0 +1,17 @@
+package com.human.ex;
+
+public class javatest01 {
+	public static void main(String[]args) {
+	System.out.println("*****************");
+	System.out.println("*     성적표      *");
+	System.out.println("*****************");
+	System.out.println("*     국어50     *");
+	System.out.println("*****************");
+	System.out.println("*     영어50     *");
+	System.out.println("*****************");
+	System.out.println("*     수학50     *");
+	System.out.println("*****************");
+	System.out.println("* 컴퓨터학과 홍길동  *");
+	System.out.println("*****************");
+	}
+}
